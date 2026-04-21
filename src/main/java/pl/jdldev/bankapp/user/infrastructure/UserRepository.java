1be119c1db6adae20a944cf74fcfcb622ad25d9d
jdl-dev <januszkiewiczdl@gmail.com>
2026-04-21 20:20:27 +0200

    Added 3 methods to UserRepository: Optional<User> findByEmail(String email); boolean existsByEmail(String email); User save(User user);

--- src/main/java/pl/jdldev/bankapp/user/infrastructure/UserRepository.java
+++ src/main/java/pl/jdldev/bankapp/user/infrastructure/UserRepository.java
@@ -3,5 +3,10 @@ package pl.jdldev.bankapp.user.infrastructure;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.jdldev.bankapp.user.domain.User;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+    User save(User user);
 }
