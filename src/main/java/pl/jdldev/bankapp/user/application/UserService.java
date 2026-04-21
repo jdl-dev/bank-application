7b7499c111ccb157a041f9e05796fb81ed45292e
jdl-dev <januszkiewiczdl@gmail.com>
2026-04-21 20:22:38 +0200

    UserService adding new customer to database.

---
+++ src/main/java/pl/jdldev/bankapp/user/application/UserService.java
@@ -0,0 +1,37 @@
+package pl.jdldev.bankapp.user.application;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import pl.jdldev.bankapp.user.api.RegisterUserRequest;
+import pl.jdldev.bankapp.user.domain.User;
+import pl.jdldev.bankapp.user.domain.UserRole;
+import pl.jdldev.bankapp.user.infrastructure.UserRepository;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Transactional
+    public void register(RegisterUserRequest registerUserRequest) {
+        if (userRepository.existsByEmail(registerUserRequest.email())) {
+            throw new IllegalArgumentException("Given email already exists.");
+        }
+
+        User user = new User(
+                registerUserRequest.email(),
+                passwordEncoder.encode(registerUserRequest.password()),
+                UserRole.CUSTOMER,
+                LocalDateTime.now(),
+                LocalDateTime.now()
+                );
+
+        userRepository.save(user);
+    }
+}
