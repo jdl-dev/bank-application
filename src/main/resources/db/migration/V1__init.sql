d989a562ef1bd4368abebb1c092526902165464d
jdl-dev <januszkiewiczdl@gmail.com>
2026-04-21 19:54:39 +0200

    First Flyway database version. Just to check if everything works.

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,9 @@
+CREATE TABLE users (
+                       id BIGSERIAL PRIMARY KEY,
+                       email VARCHAR(255) NOT NULL UNIQUE,
+                       password_hash VARCHAR(255) NOT NULL,
+                       role VARCHAR(50) NOT NULL,
+                       status VARCHAR(50) NOT NULL,
+                       created_at TIMESTAMP NOT NULL,
+                       updated_at TIMESTAMP NOT NULL
+);
