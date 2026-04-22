86afd7166c999bfdf9e5f47403934f446097ed3f
jdl-dev <januszkiewiczdl@gmail.com>
2026-04-22 13:34:47 +0200

    Change message from BankAccountNotFoundException, better readability.

--- src/main/java/pl/jdldev/bankapp/common/exception/BankAccountNotFoundException.java
+++ src/main/java/pl/jdldev/bankapp/common/exception/BankAccountNotFoundException.java
@@ -2,6 +2,6 @@ package pl.jdldev.bankapp.common.exception;
 
 public class BankAccountNotFoundException extends  RuntimeException {
     public BankAccountNotFoundException(Long id) {
-        super("Could not find account with '%s'".formatted(id));
+        super("Could not find account with id: '%s'".formatted(id));
     }
 }
