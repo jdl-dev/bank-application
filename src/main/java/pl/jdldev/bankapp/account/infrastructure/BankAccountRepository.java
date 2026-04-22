d4cb79e68e0f0ec15ece475a6d6042e4772304a6
jdl-dev <januszkiewiczdl@gmail.com>
2026-04-22 13:09:26 +0200

    Removed unnecsseary save() method.

--- src/main/java/pl/jdldev/bankapp/account/infrastructure/BankAccountRepository.java
+++ src/main/java/pl/jdldev/bankapp/account/infrastructure/BankAccountRepository.java
@@ -1,8 +1,8 @@
 package pl.jdldev.bankapp.account.infrastructure;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import pl.jdldev.bankapp.account.domain.BankAccount;
 
-public interface BankAccountRepository {
+public interface BankAccountRepository extends JpaRepository<BankAccount, Long> {
     boolean existsByAccountNumber(String accountNumber);
-    BankAccount save(BankAccount bankAccount);
 }
