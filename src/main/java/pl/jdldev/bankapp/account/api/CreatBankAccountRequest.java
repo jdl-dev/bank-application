4f884cf83124fadb9b5b8031c14bee30ef8a2074
jdl-dev <januszkiewiczdl@gmail.com>
2026-04-22 12:55:26 +0200

    Added validation constraints to CreatBankAccountRequest.

--- src/main/java/pl/jdldev/bankapp/account/api/CreatBankAccountRequest.java
+++ src/main/java/pl/jdldev/bankapp/account/api/CreatBankAccountRequest.java
@@ -1,8 +1,12 @@
 package pl.jdldev.bankapp.account.api;
 
+import jakarta.validation.constraints.NotNull;
 import pl.jdldev.bankapp.account.domain.CurrencyCode;
 
 public record CreatBankAccountRequest (
+        @NotNull
         Long ownerId,
+
+        @NotNull
         CurrencyCode currency
 ){}
