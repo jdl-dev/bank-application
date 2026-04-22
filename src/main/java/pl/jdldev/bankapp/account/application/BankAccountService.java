5791eb25246cd6008f584a997d849b1bf4b3ab08
jdl-dev <januszkiewiczdl@gmail.com>
2026-04-22 12:48:45 +0200

    Create bank account service method with necessary method in repository.

--- src/main/java/pl/jdldev/bankapp/account/application/BankAccountService.java
+++ src/main/java/pl/jdldev/bankapp/account/application/BankAccountService.java
@@ -2,13 +2,57 @@ package pl.jdldev.bankapp.account.application;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import pl.jdldev.bankapp.account.api.BankAccountResponse;
+import pl.jdldev.bankapp.account.api.CreatBankAccountRequest;
+import pl.jdldev.bankapp.account.domain.AccountStatus;
 import pl.jdldev.bankapp.account.domain.BankAccount;
 import pl.jdldev.bankapp.account.infrastructure.BankAccountRepository;
+import pl.jdldev.bankapp.account.mapper.BankAccountMapper;
+import pl.jdldev.bankapp.common.exception.UserNotFoundException;
+import pl.jdldev.bankapp.user.domain.User;
+import pl.jdldev.bankapp.user.infrastructure.UserRepository;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class BankAccountService {
 
     private final BankAccountRepository bankAccountRepository;
+    private final UserRepository userRepository;
+    private final BankAccountMapper bankAccountMapper;
 
+    public BankAccountResponse createBankAccount(CreatBankAccountRequest createBankAccountRequest) {
+
+        User accountOwner = userRepository
+                .getUserById(createBankAccountRequest.ownerId())
+                .orElseThrow(() -> new UserNotFoundException(createBankAccountRequest.ownerId()));
+
+        BankAccount newBankAccount = new BankAccount(
+                generateBankAccountNumber(),
+                createBankAccountRequest.currency(),
+                BigDecimal.ZERO,
+                AccountStatus.ACTIVE,
+                accountOwner,
+                LocalDateTime.now(),
+                LocalDateTime.now()
+        );
+
+        BankAccount savedBankAccount = bankAccountRepository.save(newBankAccount);
+
+        return bankAccountMapper.toResponse(savedBankAccount);
+    }
+
+    private String generateBankAccountNumber() {
+
+        String accountNumber;
+
+        do {
+            accountNumber = UUID.randomUUID().toString();
+        } while (bankAccountRepository.existsByAccountNumber(accountNumber));
+
+        return accountNumber;
+    }
 }
--- src/main/java/pl/jdldev/bankapp/account/infrastructure/BankAccountRepository.java
+++ src/main/java/pl/jdldev/bankapp/account/infrastructure/BankAccountRepository.java
@@ -1,4 +1,8 @@
 package pl.jdldev.bankapp.account.infrastructure;
 
+import pl.jdldev.bankapp.account.domain.BankAccount;
+
 public interface BankAccountRepository {
+    boolean existsByAccountNumber(String accountNumber);
+    BankAccount save(BankAccount bankAccount);
 }
