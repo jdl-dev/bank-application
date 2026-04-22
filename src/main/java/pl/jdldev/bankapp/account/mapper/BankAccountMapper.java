7a0d553817a8c11e49c399ba0ac9d4d4ee74b121
jdl-dev <januszkiewiczdl@gmail.com>
2026-04-22 12:34:08 +0200

    Mapper for BankAccount from entity to DTO.

---
+++ src/main/java/pl/jdldev/bankapp/account/mapper/BankAccountMapper.java
@@ -0,0 +1,19 @@
+package pl.jdldev.bankapp.account.mapper;
+
+import pl.jdldev.bankapp.account.api.BankAccountResponse;
+import pl.jdldev.bankapp.account.domain.BankAccount;
+
+public class BankAccountMapper {
+    public BankAccountResponse toResponse(BankAccount bankAccount) {
+        return new BankAccountResponse(
+                bankAccount.getId(),
+                bankAccount.getAccountNumber(),
+                bankAccount.getCurrency(),
+                bankAccount.getBalance(),
+                bankAccount.getStatus(),
+                bankAccount.getOwner().getId(),
+                bankAccount.getCreatedAt(),
+                bankAccount.getUpdatedAt()
+        );
+    }
+}
