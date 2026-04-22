8dae02549b8aa806d163c3622ab0e74a411f25c8
jdl-dev <januszkiewiczdl@gmail.com>
2026-04-22 13:10:32 +0200

    Added @Component annotation beacuse spring didn't see it.

--- src/main/java/pl/jdldev/bankapp/account/mapper/BankAccountMapper.java
+++ src/main/java/pl/jdldev/bankapp/account/mapper/BankAccountMapper.java
@@ -1,8 +1,10 @@
 package pl.jdldev.bankapp.account.mapper;
 
+import org.springframework.stereotype.Component;
 import pl.jdldev.bankapp.account.api.BankAccountResponse;
 import pl.jdldev.bankapp.account.domain.BankAccount;
 
+@Component
 public class BankAccountMapper {
     public BankAccountResponse toResponse(BankAccount bankAccount) {
         return new BankAccountResponse(
