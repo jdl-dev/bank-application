10060e2a61e0b8b5be1c4c3e51f6ba1ea3e6381a
jdl-dev <januszkiewiczdl@gmail.com>
2026-04-22 12:09:41 +0200

    One more time setting .anyRequest().denyAll()) -> .anyRequest().permitAll()) for esear development.

--- src/main/java/pl/jdldev/bankapp/security/config/SecurityConfig.java
+++ src/main/java/pl/jdldev/bankapp/security/config/SecurityConfig.java
@@ -17,7 +17,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/v1/users/register").permitAll()
                         .requestMatchers("/api/v1/users/*").permitAll()
-                        .anyRequest().denyAll())
+                        .anyRequest().permitAll())
                 .build();
     }
 }
