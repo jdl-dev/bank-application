4c287a48939abc88bbb8c8a4953f9816428d3d20
jdl-dev <januszkiewiczdl@gmail.com>
2026-04-22 13:10:54 +0200

    Created method in controller to create bank account.

--- src/main/java/pl/jdldev/bankapp/account/api/BankAccountController.java
+++ src/main/java/pl/jdldev/bankapp/account/api/BankAccountController.java
@@ -1,12 +1,21 @@
 package pl.jdldev.bankapp.account.api;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
-import pl.jdldev.bankapp.account.infrastructure.BankAccountRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import pl.jdldev.bankapp.account.application.BankAccountService;
 
 @RestController
+@RequestMapping("/api/v1/accounts")
 @RequiredArgsConstructor
 public class BankAccountController {
 
-    private final BankAccountRepository bankAccountRepository;
+    private final BankAccountService bankAccountService;
+
+    @PostMapping()
+    @ResponseStatus(HttpStatus.CREATED)
+    public BankAccountResponse createBankAccount(@Valid @RequestBody CreatBankAccountRequest request) {
+        return bankAccountService.createBankAccount(request);
+    }
 }
