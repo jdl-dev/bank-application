245729bf64073f6ab09fb8e075345f8f793cdcd9
jdl-dev <januszkiewiczdl@gmail.com>
2026-05-04 19:25:25 +0200

    Create transaction method in controller and service.

--- src/main/java/pl/jdldev/bankapp/transaction/api/TransactionController.java
+++ src/main/java/pl/jdldev/bankapp/transaction/api/TransactionController.java
@@ -1,10 +1,23 @@
 package pl.jdldev.bankapp.transaction.api;
 
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.jdldev.bankapp.transaction.application.TransactionService;
+import pl.jdldev.bankapp.transaction.domain.Transaction;
 
 @RestController
 @RequestMapping("/api/v1/transactions")
+@RequiredArgsConstructor
 public class TransactionController {
 
+    private final TransactionService transactionService;
+
+    @PostMapping
+    public Transaction createTransaction(@Valid @RequestBody CreateTransactionRequest createTransactionRequest) {
+        return transactionService.createTransaction(createTransactionRequest);
+    }
 }
--- src/main/java/pl/jdldev/bankapp/transaction/application/TransactionService.java
+++ src/main/java/pl/jdldev/bankapp/transaction/application/TransactionService.java
@@ -1,4 +1,80 @@
 package pl.jdldev.bankapp.transaction.application;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import pl.jdldev.bankapp.account.domain.AccountStatus;
+import pl.jdldev.bankapp.account.domain.BankAccount;
+import pl.jdldev.bankapp.account.domain.CurrencyCode;
+import pl.jdldev.bankapp.common.exception.WrongTransactionDestinationException;
+import pl.jdldev.bankapp.account.infrastructure.BankAccountRepository;
+import pl.jdldev.bankapp.common.exception.BankAccountNotFoundException;
+import pl.jdldev.bankapp.common.exception.TooSmallAccountBalanceToMakeTransaction;
+import pl.jdldev.bankapp.transaction.api.CreateTransactionRequest;
+import pl.jdldev.bankapp.transaction.domain.Transaction;
+import pl.jdldev.bankapp.transaction.infrastructure.TransactionRepository;
+
+import java.math.BigDecimal;
+
+@Service
+@RequiredArgsConstructor
 public class TransactionService {
+
+    private final TransactionRepository transactionRepository;
+    private final BankAccountRepository bankAccountRepository;
+
+    public Transaction createTransaction(CreateTransactionRequest createTransactionRequest) {
+
+        Long sourceAccountId = createTransactionRequest.sourceAccountId();
+        Long destinationAccountId = createTransactionRequest.destinationAccountId();
+
+        BankAccount sourceBankAccount = bankAccountRepository
+                .getBankAccountById(sourceAccountId)
+                .orElseThrow(() -> new BankAccountNotFoundException(sourceAccountId));
+
+        BankAccount destinationBankAccount = bankAccountRepository
+                .getBankAccountById(destinationAccountId)
+                .orElseThrow(() -> new BankAccountNotFoundException(destinationAccountId));
+
+        if(!isSourceAccountNotSameAsDestinationAccount(sourceAccountId, destinationAccountId)) {
+            throw new WrongTransactionDestinationException(sourceAccountId, destinationAccountId);
+        }
+
+        BigDecimal accountBalance = sourceBankAccount.getBalance();
+        BigDecimal transactionAmount = createTransactionRequest.amount();
+
+        if(!isAccountBalanceGreaterThanTransactionAmount(accountBalance, transactionAmount)) {
+            throw new TooSmallAccountBalanceToMakeTransaction(accountBalance, transactionAmount);
+        }
+
+        AccountStatus sourceAccountStatus = sourceBankAccount.getStatus();
+        AccountStatus destinationAccountStatus = destinationBankAccount.getStatus();
+
+        if(!isAccountStatusActive(sourceAccountStatus)){
+            throw
+        }
+
+        if(!isAccountStatusActive(destinationAccountStatus)) {
+
+        }
+
+    }
+
+
+    private boolean isSourceAccountNotSameAsDestinationAccount(Long sourceAccountId, Long destinationAccountId){
+        return sourceAccountId.equals(destinationAccountId);
+    }
+
+    private boolean isAccountBalanceGreaterThanTransactionAmount(BigDecimal balance, BigDecimal transactionAmount) {
+        return balance.compareTo(transactionAmount) > 0;
+    }
+
+    private boolean isAccountStatusActive(AccountStatus  accountStatus) {
+        return accountStatus.equals(AccountStatus.ACTIVE);
+    }
+
+    private boolean isCurrencyTransactionSameOnBothAccounts(CurrencyCode sourceAccount, CurrencyCode destinationAccount) {
+        return sourceAccount.equals(destinationAccount);
+    }
+
+
 }
